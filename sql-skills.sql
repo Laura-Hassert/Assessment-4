8e6a07c837edd6691b8ca9cadd7a47c6d618cc48
Laura-Hassert <laura.hassert@gmail.com>
2021-09-24 12:08:01 -0400

    extra credit sql

--- sql-skills.sql
+++ sql-skills.sql
@@ -1,16 +1,16 @@
------ ARTIST TABLE ----- 
+------------------ ARTIST TABLE ------------------
 1. 
 INSERT INTO artist (name)
 	VALUES ('Angele'),
-  ('Lady Gaga'),
-  ('Lizzo');
+    ('Lady Gaga'),
+    ('Lizzo');
 
 2. 
 SELECT name FROM artist
 ORDER BY name
 LIMIT 5;
 
------ EMPLOYEE TABLE ----- 
+------------------ EMPLOYEE TABLE ------------------
 1. 
 SELECT first_name, last_name FROM employee
 WHERE city = 'Calgary';
@@ -23,7 +23,7 @@ WHERE reports_to = 2;
 SELECT COUNT(*) FROM employee
 WHERE city = 'Lethbridge';
 
------ INVOICE TABLE ----- 
+------------------ INVOICE TABLE ------------------
 1. 
 SELECT COUNT(*) FROM invoice
 WHERE billing_country = 'USA';
@@ -45,7 +45,7 @@ WHERE total < 5;
 6. 
 SELECT SUM(total) FROM invoice;
 
------ JOIN QUERIES ----- 
+------------------ JOIN QUERIES ------------------
 1. 
 SELECT * FROM invoice i
 JOIN invoice_line il ON i.invoice_id = il.invoice_id
@@ -66,3 +66,57 @@ JOIN customer c ON e.employee_id = c.support_rep_id;
 4. 
 SELECT al.title AS album_title, ar.name AS artist_name FROM album al
 JOIN artist ar ON al.artist_id = ar.artist_id;
+
+
+---------------------------------------------------------- EXTRA CREDIT -----------------------------------------------------------
+
+------------------ ARTIST TABLE ------------------
+1. 
+SELECT * FROM artist
+ORDER BY name DESC
+LIMIT 10;
+
+2. 
+SELECT * FROM artist
+WHERE name LIKE 'Black%';
+
+3.
+SELECT * FROM artist
+WHERE name ILIKE '%black%';
+
+------------------ EMPLOYEE TABLE ------------------
+1. 
+SELECT MAX(birth_date) FROM employee;
+
+2. 
+SELECT MIN(birth_date) FROM employee;
+
+------------------ INVOICE TABLE ------------------
+1. 
+SELECT COUNT(*) FROM invoice
+WHERE billing_state IN ('CA', 'TX', 'AZ');
+
+2. 
+SELECT AVG(total) FROM invoice;
+
+------------------JOIN QUERIES ------------------
+1. 
+SELECT pt.playlist_track_id, pt.track_id FROM playlist_track pt
+JOIN playlist p ON pt.playlist_id = p.playlist_id
+WHERE p.name = 'Music';
+
+2. 
+SELECT t.name FROM track t
+JOIN playlist_track pt ON pt.track_id = t.track_id
+WHERE pt.playlist_id = 5;
+
+3. 
+SELECT t.name AS track_name, p.name AS playlist_name FROM track t
+JOIN playlist_track pt ON pt.track_id = t.track_id
+JOIN playlist p ON pt.playlist_id = p.playlist_id;
+
+4. 
+SELECT t.name AS track_name, a.title AS album_title FROM track t
+JOIN album a ON a.album_id = t.album_id
+JOIN genre g ON g.genre_id = t.genre_id
+WHERE g.name = 'Alternative & Punk';
